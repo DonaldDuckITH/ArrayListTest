c5b64ff9de5002c0d3b0594a7e6066eec89ac31f
DonaldDuckITH <58251441+DonaldDuckITH@users.noreply.github.com>
2022-06-10 08:56:25 +1000

    Add files via upload

---
+++ Project2/src/net/donaldduckith/project/Fields.java
@@ -0,0 +1,28 @@
+package net.donaldduckith.project;
+
+import com.google.gson.Gson;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Assert;
+
+public class Fields extends Main {
+    private int id;
+    private String name;
+
+    public Fields (int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void givenListOfFields_whenSerializing_thenCorrect() {
+        List<Fields> list = Arrays.asList(new Fields(1, "name1"), new Fields(2, "name2"));
+
+        Gson gson = new Gson();
+        String jsonString = list.toString();
+        String expectedString = "[{\"id\":1,\"name\":\"name1\"},{\"id\":2,\"name\":\"name2\"}]";
+
+        Assert.assertEquals(expectedString, jsonString);
+
+    }
+}
---
+++ Project2/src/net/donaldduckith/project/Main.java
@@ -0,0 +1,69 @@
+package net.donaldduckith.project;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
+import com.google.gson.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String sURL = "https://jsonplaceholder.typicode.com/posts/1/comments";
+
+        try {
+            URL url = new URL(sURL);
+            URLConnection request = url.openConnection();
+            request.connect();
+
+            @Deprecated
+            JsonParser jp = new JsonParser();
+            @Deprecated
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+            JsonArray rootObj = root.getAsJsonArray();
+            //System.out.println(rootObj.get());
+            System.out.println(sURL);
+            //System.out.println(rootObj);
+            //System.out.println(rootObj.get(1));
+            System.out.println("Size of JSONArray = " + rootObj.size());
+
+
+            for (int i = 0; i < rootObj.size(); ++i) {
+                //System.out.println(rootObj.get(i));
+                JsonElement je = rootObj.get(i);
+                System.out.println(je);
+
+                for (String field : je.getAsJsonObject().keySet()) {
+                    JsonObject jo = je.getAsJsonObject();
+                    System.out.println(field + "=" + jo.get(field));
+                }
+            }
+
+            String sURL2 = "https://jsonplaceholder.typicode.com/comments";
+
+            URL url2 = new URL(sURL2);
+            URLConnection request2 = url2.openConnection();
+            request2.connect();
+
+            @Deprecated
+            JsonParser jp2 = new JsonParser();
+            @Deprecated
+            JsonElement root2 = jp2.parse(new InputStreamReader((InputStream) request2.getContent()));
+            JsonArray rootObj2 = root2.getAsJsonArray();
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please enter a postId.");
+
+            int input = scanner.nextInt();
+
+
+            for (String field : rootObj2.getAsJsonObject().keySet()) {
+
+            }
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
+
+    }
+}
